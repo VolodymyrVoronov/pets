458485cea5fbd643165feb747d5a3c850f309acd
Volodymyr Voronov <malones.vladimir@gmail.com>
2022-12-25 14:51:44 +0100

    chore(components): updaeted NavBar.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -19,7 +19,7 @@ const NavBar = ({
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 2 }}
+      transition={{ duration: 1 }}
     >
       <div className={cn(styles.container, className)} {...props}>
         {children}
