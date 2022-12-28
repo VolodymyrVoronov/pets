4be152c18f07802c2e97a7788d48354bb1e9947b
Volodymyr Voronov <malones.vladimir@gmail.com>
2022-12-28 14:10:39 +0100

    chore(services): fixed wrong path.

--- src/services/api.ts
+++ src/services/api.ts
@@ -47,7 +47,7 @@ const addPet = async (newPet: IAddPet): Promise<IPet | string> => {
 
 const deletePet = async (id: number): Promise<IPet | string> => {
   try {
-    const { data } = await axios.delete(`${url}/petsx/${id}`);
+    const { data } = await axios.delete(`${url}/pets/${id}`);
 
     return data as IPet;
   } catch (error) {
