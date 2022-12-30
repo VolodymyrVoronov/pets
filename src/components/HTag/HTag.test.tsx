d030fb15a501cb3e559c30b2b8664f6f41a6dced
Volodymyr Voronov <malones.vladimir@gmail.com>
2022-12-30 12:36:34 +0100

    chore(components): updated tests for HTag.

--- src/components/HTag/HTag.test.tsx
+++ src/components/HTag/HTag.test.tsx
@@ -3,6 +3,20 @@ import { getByText, render } from "@testing-library/react";
 import HTag from "./HTag";
 
 describe("HTag", () => {
+  it("renders correctly", () => {
+    const { container } = render(<HTag>Test text</HTag>);
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <h1
+          class="_hTag_1c0db3 _h1_1c0db3"
+        >
+          Test text
+        </h1>
+      </div>
+    `);
+  });
+
   it("should render default tag h1.", () => {
     const { container } = render(<HTag>Test text</HTag>);
 
@@ -11,7 +25,7 @@ describe("HTag", () => {
     expect(tag).toBeInTheDocument();
   });
 
-  it("should render passed tag h1.", () => {
+  it("should render tag h1.", () => {
     const { container } = render(<HTag tag="h1">Test text</HTag>);
 
     const tag = container.querySelector("h1");
@@ -19,7 +33,7 @@ describe("HTag", () => {
     expect(tag).toBeInTheDocument();
   });
 
-  it("should render passed tag h2.", () => {
+  it("should render tag h2.", () => {
     const { container } = render(<HTag tag="h2">Test text</HTag>);
 
     const tag = container.querySelector("h2");
@@ -27,7 +41,7 @@ describe("HTag", () => {
     expect(tag).toBeInTheDocument();
   });
 
-  it("should render passed tag h3.", () => {
+  it("should render tag h3.", () => {
     const { container } = render(<HTag tag="h3">Test text</HTag>);
 
     const tag = container.querySelector("h3");
@@ -35,7 +49,7 @@ describe("HTag", () => {
     expect(tag).toBeInTheDocument();
   });
 
-  it("should render passed tag h4.", () => {
+  it("should render tag h4.", () => {
     const { container } = render(<HTag tag="h4">Test text</HTag>);
 
     const tag = container.querySelector("h4");
@@ -43,7 +57,7 @@ describe("HTag", () => {
     expect(tag).toBeInTheDocument();
   });
 
-  it("should render passed tag h5.", () => {
+  it("should render tag h5.", () => {
     const { container } = render(<HTag tag="h5">Test text</HTag>);
 
     const tag = container.querySelector("h5");
@@ -51,7 +65,7 @@ describe("HTag", () => {
     expect(tag).toBeInTheDocument();
   });
 
-  it("should render passed tag h6.", () => {
+  it("should render tag h6.", () => {
     const { container } = render(<HTag tag="h6">Test text</HTag>);
 
     const tag = container.querySelector("h6");
@@ -59,7 +73,7 @@ describe("HTag", () => {
     expect(tag).toBeInTheDocument();
   });
 
-  it("should find passed className.", () => {
+  it("should find className.", () => {
     const testClassName = "test-class-name";
 
     const { container } = render(
@@ -69,7 +83,7 @@ describe("HTag", () => {
     expect(container.getElementsByClassName(testClassName).length).toBe(1);
   });
 
-  it("should find text passed via children.", () => {
+  it("should find text.", () => {
     const textText = "Test text";
 
     const { container } = render(<HTag>{textText}</HTag>);
