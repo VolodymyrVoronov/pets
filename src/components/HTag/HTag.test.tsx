f976fa31938106ee1befaece132a33d4bf581658
Volodymyr Voronov <malones.vladimir@gmail.com>
2022-12-30 09:27:28 +0100

    chore(components): updated tests for HTag.

--- src/components/HTag/HTag.test.tsx
+++ src/components/HTag/HTag.test.tsx
@@ -11,6 +11,14 @@ describe("HTag", () => {
     expect(tag).toBeInTheDocument();
   });
 
+  it("should render passed tag h1.", () => {
+    const { container } = render(<HTag tag="h1">Test text</HTag>);
+
+    const tag = container.querySelector("h1");
+
+    expect(tag).toBeInTheDocument();
+  });
+
   it("should render passed tag h2.", () => {
     const { container } = render(<HTag tag="h2">Test text</HTag>);
 
@@ -19,12 +27,36 @@ describe("HTag", () => {
     expect(tag).toBeInTheDocument();
   });
 
-  it("should not find tag h1, because tag h3 was passed via props.", () => {
+  it("should render passed tag h3.", () => {
     const { container } = render(<HTag tag="h3">Test text</HTag>);
 
-    const tag = container.querySelector("h1");
+    const tag = container.querySelector("h3");
 
-    expect(tag).not.toBeInTheDocument();
+    expect(tag).toBeInTheDocument();
+  });
+
+  it("should render passed tag h4.", () => {
+    const { container } = render(<HTag tag="h4">Test text</HTag>);
+
+    const tag = container.querySelector("h4");
+
+    expect(tag).toBeInTheDocument();
+  });
+
+  it("should render passed tag h5.", () => {
+    const { container } = render(<HTag tag="h5">Test text</HTag>);
+
+    const tag = container.querySelector("h5");
+
+    expect(tag).toBeInTheDocument();
+  });
+
+  it("should render passed tag h6.", () => {
+    const { container } = render(<HTag tag="h6">Test text</HTag>);
+
+    const tag = container.querySelector("h6");
+
+    expect(tag).toBeInTheDocument();
   });
 
   it("should find passed className.", () => {
